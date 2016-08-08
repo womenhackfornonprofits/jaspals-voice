23fb23e657b95ce4661a19fb964a1759a0a8ec0e
Aastha Malviya <amalviya@isystango.com>
2016-08-08 15:23:24 +0530

    AboutMe: UI changes and single save

--- app/src/main/java/uk/co/jaspalsvoice/jv/activities/LikesDislikesActivity.java
+++ app/src/main/java/uk/co/jaspalsvoice/jv/activities/LikesDislikesActivity.java
@@ -1,44 +1,88 @@
 package uk.co.jaspalsvoice.jv.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
+import uk.co.jaspalsvoice.jv.CommonUtils;
 import uk.co.jaspalsvoice.jv.R;
-import uk.co.jaspalsvoice.jv.views.EditableCardView;
 
 /**
  * Created by Ana on 2/8/2016.
  */
 public class LikesDislikesActivity extends BaseActivity {
 
-    @Override
+    private LinearLayout mainLayoutAboutMe;
+    private LinearLayout buttonLayout;
+    private EditText dailyRoutine;
+    private EditText interestHobbies;
+    private EditText music;
+    private EditText television;
+    private EditText other;
+    private Button save;
+    private Button cancel;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_likes_dislikes);
+        initViews();
+        attachListeners();
+        loadData();
+    }
 
-        EditableCardView dailyRoutine = (EditableCardView) findViewById(R.id.daily_routine);
-        dailyRoutine.setTitle(getString(R.string.likes_dislikes_daily_routine));
-        dailyRoutine.setTitleId(R.string.likes_dislikes_daily_routine);
+    private void initViews() {
+        dailyRoutine = (EditText) findViewById(R.id.editDailyRoutine);
+        interestHobbies = (EditText) findViewById(R.id.editInterstHobbies);
+        music = (EditText) findViewById(R.id.editMusic);
+        television = (EditText) findViewById(R.id.editTelevision);
+        other = (EditText) findViewById(R.id.editOther);
+        save = (Button) findViewById(R.id.save);
+        cancel = (Button) findViewById(R.id.cancel);
+        mainLayoutAboutMe = (LinearLayout) findViewById(R.id.mainLayoutAboutMe);
+        buttonLayout = (LinearLayout) findViewById(R.id.buttons);
+    }
+
+    private void attachListeners() {
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CommonUtils.hideKeyboard(LikesDislikesActivity.this);
+                preferences.setLikesDislikesRoutine(dailyRoutine.getText().toString().trim());
+                preferences.setLikesDislikesHobbies(interestHobbies.getText().toString().trim());
+                preferences.setLikesDislikesMusic(music.getText().toString().trim());
+                preferences.setLikesDislikesTelevision(television.getText().toString().trim());
+                preferences.setLikesDislikesOther(other.getText().toString().trim());
+                mainLayoutAboutMe.requestFocus();
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadData();
+                mainLayoutAboutMe.requestFocus();
+                CommonUtils.hideKeyboard(LikesDislikesActivity.this);
+            }
+        });
+        mainLayoutAboutMe.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    buttonLayout.setVisibility(View.GONE);
+                } else {
+                    buttonLayout.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+
+    private void loadData() {
         dailyRoutine.setText(preferences.getLikesDislikesRoutine());
-
-        EditableCardView hobbies = (EditableCardView) findViewById(R.id.hobbies);
-        hobbies.setTitle(getString(R.string.likes_dislikes_hobbies));
-        hobbies.setTitleId(R.string.likes_dislikes_hobbies);
-        hobbies.setText(preferences.getLikesDislikesHobbies());
-
-        EditableCardView music = (EditableCardView) findViewById(R.id.music);
-        music.setTitle(getString(R.string.likes_dislikes_music));
-        music.setTitleId(R.string.likes_dislikes_music);
+        interestHobbies.setText(preferences.getLikesDislikesHobbies());
         music.setText(preferences.getLikesDislikesMusic());
-
-        EditableCardView tv = (EditableCardView) findViewById(R.id.tv);
-        tv.setTitle(getString(R.string.likes_dislikes_tv));
-        tv.setTitleId(R.string.likes_dislikes_tv);
-        tv.setText(preferences.getLikesDislikesTelevision());
-
-        EditableCardView other = (EditableCardView) findViewById(R.id.other);
-        other.setTitle(getString(R.string.likes_dislikes_other));
-        other.setTitleId(R.string.likes_dislikes_other);
+        television.setText(preferences.getLikesDislikesTelevision());
         other.setText(preferences.getLikesDislikesOther());
     }
 }
